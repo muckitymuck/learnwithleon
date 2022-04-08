77cd3f1e94620316247f0e9a8f5efa03e9d6aef8
muckity <andrewgoldstein@hotmail.com>
2022-04-07 19:55:16 -0500

    objects

--- class26-materials/objects-constructor-fun/js/main.js
+++ class26-materials/objects-constructor-fun/js/main.js
@@ -1,1 +1,20 @@
 //Create a constructor with 4 properties and 3 methods
+
+function PizzaMachine(size, toppings, crust, sauce){
+    this.size = size;
+    this.toppings = toppings;
+    this.crust = crust;
+    this.sauce = sauce;
+
+    this.estimatedDeliveryTime = function(){
+        console.log("Calculating...")
+    }
+    this.burnMouth = function() {
+        console.log("slkajfjadfsdkaj")
+    }
+    this.frisbee = function() {
+        console.log("YEEETT")
+    }
+}
+
+let pizza = new PizzaMachine('large', ['spinach', 'garlic'], 'thicc', 'white')
--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,1 +1,19 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+
+let pizza =  {}
+pizza.size = 'large';
+pizza.toppings = ['cheese', 'sauce', 'pepperoni'];
+pizza.crust = 'thin';
+pizza.sauce = 'heavy';
+
+pizza.estimatedDeliveryTime = function(){
+    console.log('Calculating...')
+}
+pizza.burnMouth = function(){
+    console.log("AFSAFDGGNBLSKFDFNAFJKNJLGSN")
+}
+
+pizza.frisbee = function(){
+    console.log("YEET")
+}
+
