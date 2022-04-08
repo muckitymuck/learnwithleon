1d02b393fe80beee228cf61394cc1e3eddb396f3
muckity <andrewgoldstein@hotmail.com>
2022-04-07 20:38:09 -0500

    cocktail init

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,1 +1,12 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+        console.log(data.drinks[0])
+         document.querySelector('h2').innerText = data.drinks[0].strDrink
+         document.querySelector('img').src = data.drinks[0].strDrinkThumb
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+});
