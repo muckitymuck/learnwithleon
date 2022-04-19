ace129f4e9d82dd8d0dc45d818ee08d458156fc0
andre <andrewgoldstein@hotmail.com>
2022-04-19 16:53:12 -0500

    memes update

--- class27-materials/class27-materials/Open Memes/js/main.js
+++ class27-materials/class27-materials/Open Memes/js/main.js
@@ -5,10 +5,10 @@ function getData() {
     fetch(`https://api.imgflip.com/get_memes`)
         .then(res => res.json()) // parse response as JSON
         .then(data => {
-            console.log(data.drinks[0])
-            document.querySelector('h2').innerText = data.drinks[0].strDrink
-            document.querySelector('img').src = data.drinks[0].strDrinkThumb
-            document.querySelector('h3').innerText = data.drinks[0].strInstructions
+            console.log(data)
+            //document.querySelector('h2').innerText = data.drinks[0].strDrink
+            //document.querySelector('img').src = data.drinks[0].strDrinkThumb
+            //document.querySelector('h3').innerText = data.drinks[0].strInstructions
         })
         .catch(err => {
             console.log(`error ${err}`)
