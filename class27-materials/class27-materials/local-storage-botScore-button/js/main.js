59978d7152930c0876de5c0bbc70ca04c39d9eb1
muckity <andrewgoldstein@hotmail.com>
2022-04-19 18:49:52 -0500

    react memes init

--- class27-materials/class27-materials/local-storage-botScore-button/js/main.js
+++ class27-materials/class27-materials/local-storage-botScore-button/js/main.js
@@ -1,3 +1,11 @@
 //Create a button that adds 1 to a botScore stored in localStorage 
+localStorage.setItem('botScore', 0)
 
+document.querySelector('button').addEventListener('click', addAnotherOne)
 
+function addAnotherOne(){
+    let botScoreVal = localStorage.getItem('botScore')
+    botScoreVal += 1
+    localStorage.setItem('botScore', botScoreVal)
+
+}
