57427a6a9da537df709f20ced2555c6b879cfcca
muckity <andrewgoldstein@hotmail.com>
2022-04-28 19:51:55 -0500

    update main.js

--- class30-materials/class30-materials/objects-espresso-machine/js/main.js
+++ class30-materials/class30-materials/objects-espresso-machine/js/main.js
@@ -1,1 +1,20 @@
 //Create an espresso machine constructor that makes machines with 4 properties and 3 methods
+class Espresso {
+    construtor(water, grind, frother, heat) {
+        this.water = water;
+        this.grind = grind;
+        this.frother = frother;
+        this.heat = heat;
+    }
+    packGrind() {
+        return this.grind;
+    }
+    startBrew() {
+        console.log(`${this.water} + " is heating." + ${this.heat}`)
+    }
+    steam(){
+        console.log("IT'S SUMMER SALE!! 50% OFF")
+    }
+
+}
+const italian = new Espresso(100, 'fine', 'frothy', 'supa hot')
